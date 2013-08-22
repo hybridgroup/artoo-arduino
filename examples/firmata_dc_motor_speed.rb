b0b60bdcf2b2ce1138c8f94936f80b2a9902ea08
Rafael Magana <raf.magana@gmail.com>
2013-08-22 10:58:38 -0500

    DC Motor example

---
+++ examples/firmata_dc_motor_speed.rb
@@ -0,0 +1,28 @@
+require 'artoo'
+
+#Circuit's breadboard layout here: http://learn.adafruit.com/adafruit-arduino-lesson-13-dc-motors/breadboard-layout
+
+#connection :firmata, :adaptor => :firmata, :port => '/dev/tty*'
+connection :firmata, :adaptor => :firmata, :port => '127.0.0.1:8023'
+device :board
+device :motor, :driver => :motor, :pin => [nil, nil, 3] # Needs a PWM pin
+
+work do
+  board.connect
+  puts "Firmware name: #{board.firmware_name}"
+  puts "Firmata version: #{board.version}"
+
+  motor.stop
+  speed = 0
+  step = 50
+
+  every 4.seconds do
+    puts "Current speed: #{speed}"
+    motor.speed speed
+    speed += step
+    if [0, 250].include?(speed)
+      step = -step
+    end
+  end
+end
+
