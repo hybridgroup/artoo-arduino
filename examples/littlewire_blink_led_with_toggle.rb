269bf970dc718615eb1270ae47d931be58c40f67
deadprogram <ron@hybridgroup.com>
2013-09-04 00:13:39 -0700

    Better description of what example includes

--- examples/littlewire_blink_led_with_toggle.rb
+++ examples/littlewire_blink_led_with_toggle.rb
@@ -1,5 +1,8 @@
 require 'artoo'
 
+# Uses Digispark USB board (http://digistump.com/products/1) 
+# with Little Wire protocol (http://littlewire.cc)
+
 connection :digispark, :adaptor => :littlewire, :vendor => 0x1781, :product => 0x0c9f
 device :board, :driver => :board
 device :led, :driver => :led, :pin => 1
