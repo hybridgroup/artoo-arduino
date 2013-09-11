a9deb11eb5d2a1a59c04febb19de9391f1ce5332
deadprogram <ron@hybridgroup.com>
2013-09-11 08:34:02 -0700

    WIP on adding i2c support to adaptor

--- lib/artoo/adaptors/littlewire.rb
+++ lib/artoo/adaptors/littlewire.rb
@@ -46,6 +46,34 @@ module Artoo
         littlewire.software_pwm_write(pin, level)
       end
 
+      # i2c interface
+      def i2c_start(address)
+        @i2c_address = address
+
+        #firmata.i2c_config(0)
+        #@i2c = littlewire.i2c
+      end
+
+      def i2c_end
+        
+      end
+
+      def i2c_read(size)
+        firmata.i2c_read_request(@i2c_address, size)
+        firmata.read_and_process
+
+        value = 0
+        while i = find_event(:i2c_reply) do
+          event = events.slice!(i)
+          value = event.data.first if !event.nil?
+        end
+        value
+      end
+
+      def i2c_write(*data)
+        firmata.i2c_write_request(@i2c_address, data)
+      end
+
       def connect_to_usb
         @usb = LIBUSB::Context.new.devices(
           :idVendor  => vendor,
