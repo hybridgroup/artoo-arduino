6557648b13648e319c9faa299363f362e8420dd3
Edgar O Silva <oswaldo.silva@gmail.com>
2014-02-05 21:53:05 -0600

    Added command registering at gem install using rake.

--- lib/artoo/commands/firmata.rb
+++ lib/artoo/commands/firmata.rb
@@ -11,7 +11,7 @@ module Artoo
         part = '-patmega328p'
         programmer = '-carduino'
         baudrate = '-b115200'
-        hex_path = File.join(File.expand_path(File.dirname(__FILE__)), "hex/StandardFirmata.cpp.hex")
+        hex_path = File.join(File.expand_path(File.dirname(__FILE__)), "StandardFirmata.cpp.hex")
         hex_file = "-Uflash:w:#{ hex_path }:i"
         port = "-P#{ options[:serial_port] }"
         case os
@@ -38,5 +38,23 @@ module Artoo
         end
       end
     end
+  end
+end
+
+# We need to register the commands in the main artoo CLI so they
+# can be picked up when this command file is required.
+# The steps needed to register new CLI commands from outside
+# artoo are:
+# 1. Related command files need to be copied to the artoo install commands.
+# 2. We do this with a rake task that is triggered when the gem is installed
+#    (see .gemspec file and look for extensions, ext/Rakefile), in the Rakefile
+#    we defined a new default class that makes use of an artoo helper class/method
+#    Artoo::Commands::Install helper method '.command'; see ext/Rakefile for details.
+# 3. Finally in our main command file (THIS FILE) we open the artoo CLI::Root class to register
+#    the new commands.
+module CLI
+  class Root
+    desc "firmata SUBCOMMAND <serial_port>", "Installs avrdude and uploads firmata to the arduino"
+    subcommand "firmata", Artoo::Commands::Firmata
   end
 end
